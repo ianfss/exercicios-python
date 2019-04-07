439008538eea6d5f87c2cf9fc6742f456f2d3d02
Davi Alves Oliveira <davi-ao@users.noreply.github.com>
2019-04-07 20:40:22 -0300

    Update ex012.py
    Informações adicionais sobre cálculo com porcentagem que podem ser úteis no futuro.

--- ex012.py
+++ ex012.py
@@ -4,3 +4,12 @@ preco = float(input('Qual o valor do produto? '))
 desconto = 5
 novopreco = preco-((desconto*preco)/100)
 print(f'O produto com o valor de {preco:.2f} sai por {novopreco:.2f} com 5% de desconto!')
+
+# Uma outra possibilidade é já colocar o desconto em forma de porcentagem que pode ser usada diretamente no cálculo.
+# 5% é o mesmo que 5/100 = 0.05
+# Então o código pode ficar assim:
+# 
+# desconto = 0.05
+# novopreco = preco - (desconto * preco)
+# 
+# com o restante igual

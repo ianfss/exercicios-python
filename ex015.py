7423db8979e1cc59db0de300d16525c34c55468f
Ian <ssianfilipe@gmail.com>
2019-04-07 20:38:06 -0300

    Correção do Exercício 015

--- ex015.py
+++ ex015.py
@@ -2,5 +2,5 @@
 
 dias = int(input('Quantos dias alugados? '))
 km = float(input('Quantos Km rodados? '))
-pago = (60.0 * a) + (0.15 * b)
+pago = (60.0 * dias) + (0.15 * km)
 print(f'O total a pagar é de R${pago}')

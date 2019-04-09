a243d61ef2e65f24de1ff61da99168331df4964d
Ian <ssianfilipe@gmail.com>
2019-04-09 13:26:17 -0300

    Update exercício 072

--- ex072.py
+++ ex072.py
@@ -1,6 +1,11 @@
 # Exercício 072 - Número por Extenso
-número = int(input('Digite um número entre 0 e 20: '))
-while número < 0 or número > 20:
-    número = int(input('Tente novamente. Digite um número entre 0 e 20: '))
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-print(f'Você digitou o número {extenso[número]}.')
+while True:
+    número = int(input('Digite um número entre 0 e 20: '))
+    if número < 0 or número > 20:
+        print('Tente novamente. ', end='')
+    elif 0 <= número <= 20:
+        print(f'Você digitou o número {extenso[número]}.')
+        continuar = str(input('Você quer continuar? [S/N] ')).strip()[0]
+        if continuar in ('nN'):
+            break

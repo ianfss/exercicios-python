87d0fd750ca7e4b89bdad5ae49e20264d0e4e394
Ian <ssianfilipe@gmail.com>
2019-04-11 20:00:51 -0300

    Update ex 075

--- ex075.py
+++ ex075.py
@@ -1,14 +1,16 @@
 # Exercício 075 - Análise de dados em uma Tupla
 
-numberOne = int(input('Digite um número: '))
-numberTwo = int(input('Digite outro número: '))
-numberThree = int(input('Digite mais um número: '))
-numberFour = int(input('Digite o último número: '))
-numbersList = (numberOne, numberTwo, numberThree, numberFour)
+numbersList = (int(input('Digite um número: ')),
+int(input('Digite outro número: ')),
+int(input('Digite mais um número: ')),
+int(input('Digite o último número: ')))
 print(f'Você digitou os valores {numbersList}')
 print(f'O valor 9 apareceu {numbersList.count(9)} vezes')
-print(f'O valor 3 apareceu na {numbersList.index(3)+1}ª posição')
-print('Os valores pares digitados foram:', end='') 
+if 3 in numbersList:
+    print(f'O valor 3 apareceu na {numbersList.index(3)+1}ª posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+print('Os valores pares digitados foram ', end='') 
 for evenNumbers in numbersList:
     if evenNumbers % 2 == 0:
-        print(f' {evenNumbers}', end='')
+        print(f'{evenNumbers}', end=' ')

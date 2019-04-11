6e9255cf376c453a4b531ee35a0db5ceadb1a62a
Ian <ssianfilipe@gmail.com>
2019-04-10 21:16:46 -0300

    Update exercício 074

--- ex074.py
+++ ex074.py
@@ -4,7 +4,5 @@ randNumbers = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), r
 print(f'Os números sorteados foram:', end=' ')
 for valueNumbers in randNumbers:
     print(f'{valueNumbers}', end=' ')
-print('')
-smallerNumber = biggerNumber = sorted(randNumbers)
-print(f'O maior valor foi {smallerNumber[0]}')
-print(f'O menor valor foi {biggerNumber[4]}')
+print(f'\nO maior número foi {max(randNumbers)}')
+print(f'O menor número foi {min(randNumbers)}')

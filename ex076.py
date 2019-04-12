913aefa4d076a80132f194697c8fb5ff265a4636
Ian <ssianfilipe@gmail.com>
2019-04-11 22:24:29 -0300

    Add 076

---
+++ ex076.py
@@ -0,0 +1,14 @@
+# Exercício 076 - Análise de dados em uma Tupla
+
+products = (('Lápis', 'Borracha', 'Caderno', 'Estojo', 'Transferidor', 'Compasso', 'Mochila', 'Canetas', 'Livro'),
+(1.75, 2.00, 15.90, 25.00, 4.20, 9.99, 120.32, 22.30, 34.90))
+print('LISTAGEM DE PREÇOS:')
+print(f'O produto "{products[0][0]}" custa R$ {products[1][0]:.2f}')
+print(f'O produto "{products[0][1]}" custa R$ {products[1][1]:.2f}')
+print(f'O produto "{products[0][2]}" custa R$ {products[1][2]:.2f}')
+print(f'O produto "{products[0][3]}" custa R$ {products[1][3]:.2f}')
+print(f'O produto "{products[0][4]}" custa R$ {products[1][4]:.2f}')
+print(f'O produto "{products[0][5]}" custa R$ {products[1][5]:.2f}')
+print(f'O produto "{products[0][6]}" custa R$ {products[1][6]:.2f}')
+print(f'O produto "{products[0][7]}" custa R$ {products[1][7]:.2f}')
+print(f'O produto "{products[0][8]}" custa R$ {products[1][8]:.2f}')

980feb5addc702a12ba4827f9acf5d39103d42fc
Ian <ssianfilipe@gmail.com>
2019-04-13 11:38:59 -0300

    Add 078

---
+++ ex078.py
@@ -0,0 +1,14 @@
+# Exercício 077 - Maior e Menor valores na Lista
+numberList = []
+for numberPosition in range(0, 5):
+    numberList.append(int(input(f'Digite um valor para a Posição {numberPosition}: ')))
+print(f'Você digitou os valores {numberList}')
+print(f'O maior valor digitado foi {max(numberList)} nas posições ', end='')
+for numberPosition, numberValue in enumerate(numberList):
+    if max(numberList) == numberValue:
+        print(f'{numberPosition}... ', end='')
+print(f'\nO menor valor digitado foi {min(numberList)} nas posições ', end='')
+for numberPosition, numberValue in enumerate(numberList):
+    if min(numberList) == numberValue:
+        print(f'{numberPosition}... ', end='')
+print('\nAcabou!')

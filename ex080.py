6f86bef779479dd17b85654c4fe459b9c90b6a22
Ian <ssianfilipe@gmail.com>
2019-04-14 09:47:38 -0300

    Add ex080.py

---
+++ ex080.py
@@ -0,0 +1,6 @@
+# Exercício 079 - Lista ordenada sem repetições
+numberList = list()
+for count in range(0, 5):
+    number = int(input('Digite um valor: '))
+    numberList.insert(number)
+print(numberList)

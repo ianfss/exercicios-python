e541256ab2840e5ad5c6afb51e91cee0ac14be96
Ian <ssianfilipe@gmail.com>
2019-04-14 15:49:41 -0300

    Update ex080

--- ex080.py
+++ ex080.py
@@ -1,6 +1,34 @@
 # Exercício 079 - Lista ordenada sem repetições
 numberList = list()
-for count in range(0, 5):
+count = 0
+while True:
+    if count == 3:
+        break
     number = int(input('Digite um valor: '))
-    numberList.insert(number)
+    if count == 0:
+        numberList.insert(0, number)
+        count += 1
+        print('Adcionado ao final da lista...')
+    elif count == 1:
+        if number > numberList[0]:
+            numberList.insert(1, number)
+            count += 1
+            print('Adcionado ao final da lista...')
+        else:
+            numberList.insert(0, number)
+            count += 1
+            print('Adcionado na posição 0...')
+    elif count == 2:
+        if number > numberList[1]:
+            numberList.insert(2, number)
+            count += 1
+            print('Adcionado ao final da lista...')
+        elif number < numberList[1] and number > numberList[0]:
+            numberList.insert(1, number)
+            count += 1
+            print('Adcionado na posição 1...')
+        elif number < numberList[0]:
+            numberList.insert(0, number)
+            count += 1
+            print('Adcionado na posição 0...')
 print(numberList)

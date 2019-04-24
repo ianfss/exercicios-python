3ced1c89e288bc9becf9cbac4342c34f30730f08
Ian <ssianfilipe@gmail.com>
2019-04-24 18:54:50 -0300

    Update 083 now using a list

--- ex083.py
+++ ex083.py
@@ -1,10 +1,16 @@
 # Exercício Python #083 - Validando expressões matemáticas
+lista = []
 expressao = str(input('Digite a expressão: '))
-abre = (expressao.count("("))
-fecha = (expressao.count(")"))
-if abre == fecha:
-    print('Sua expressão está correta!')
+for parenteses in expressao:
+    if parenteses == '(':
+        lista.append('(')
+    elif parenteses == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print ('Sua expressão está válida!')
 else:
-    print('Sua expressão está errada!')
-
-# (a+b)*c
+    print ('Sua expressão está errada!')
